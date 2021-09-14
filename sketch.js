fcf1770502909d533770f33229c859e6570f619e
shubhamwhj <75826441+shubhamwhj@users.noreply.github.com>
2021-09-14 16:33:11 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,8 +3,13 @@ var seaImg,shipImg;
 
 function preload(){
   seaImg = loadImage("sea.png");
-  shipImg1 = loadAnimation("ship-1.png","ship-1.png",
-                            "ship-2.png","ship-1.png");
+  
+  //Comment the line below and choose the correct option by uncommenting the right line to load the animation for ship
+  shipImg1=loadAnimation("sea.png")
+  //shipImg1 = loadAnimation("ship-1.png","ship-1.png","ship-2.png","ship-1.png");
+  //shipImg1 = loadAnimation("ship-1","ship-1","ship-2","ship-1");
+  //shipImg1 = loadAnimation("1.png","2.png","3.png","4.png");
+
 }
 
 function setup(){
@@ -25,7 +30,13 @@ function setup(){
 
 function draw() {
   background(0);
-  sea.velocityX = -3;
+  
+  //Uncomment the correct line to make the background move to create a forward moving effect for the boat.
+  //sea.velocityY = -3;
+  //sea.x = -3;
+  //sea.velocityX = -3;
+  //sea.velocityX = 3;
+  
 
   
     
